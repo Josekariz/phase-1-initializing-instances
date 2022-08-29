bcffaf3f18c4a15ffc1d88951c922c2e4b625fda
Josekariz <joseph.macharia@student.moringaschool.com>
2022-08-29 08:52:08 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+//let davisMeal = new breakfast('Ugali', 'tea');
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dinner{
+  #dessert
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.#dessert = dessert;
+  }
+}
